d24dc919e35b2e30ee0ac95fc4ccf0a5f8db3a6c
Ilya Sokolov <developer.sokolov@gmail.com>
2024-01-28 03:51:28 +0300

    add base GameApp

---
+++ app/src/main/java/org/redbyte/genom/ui/GameApp.kt
@@ -0,0 +1,25 @@
+package org.redbyte.genom.ui
+
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import kotlinx.coroutines.delay
+import org.redbyte.genom.GameLogic
+
+@Composable
+fun GameApp() {
+    val gameLogic = remember { GameLogic(10, 10) }
+    val gameState = remember { mutableStateOf(gameLogic.cells) }
+
+    BoxWithConstraints(Modifier.fillMaxSize()) {
+        LaunchedEffect(Unit) {
+            while (true) {
+                delay(1000)
+                gameLogic.nextTurn()
+                gameState.value = gameLogic.cells
+            }
+        }
+        GameBoard(gameState.value.map { it.toList() })
+    }
+}
