33326151ae9e2fd676ca7a9a299d654406014839
Ilya Sokolov <developer.sokolov@gmail.com>
2024-01-28 03:49:34 +0300

    add base GameBoard

---
+++ app/src/main/java/org/redbyte/genom/ui/GameBoard.kt
@@ -0,0 +1,24 @@
+package org.redbyte.genom.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.unit.dp
+import org.redbyte.genom.Cell
+
+@Composable
+fun GameBoard(cells: List<List<Cell>>) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(cells[0].size),
+        contentPadding = PaddingValues(4.dp),
+        horizontalArrangement = Arrangement.spacedBy(4.dp),
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        items(cells.flatten()) { cell ->
+            CellView(cell)
+        }
+    }
+}
