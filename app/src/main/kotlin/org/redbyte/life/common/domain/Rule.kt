7ce6f6efce02099a7e560258cba072ae3e8a92aa
Ilya Sokolov <developer.sokolov@gmail.com>
2024-09-10 18:13:15 +0300

    add new rule: HighLifeRule

--- app/src/main/kotlin/org/redbyte/life/common/domain/Rule.kt
+++ app/src/main/kotlin/org/redbyte/life/common/domain/Rule.kt
@@ -13,3 +13,8 @@ data object DayAndNightRule : Rule {
     override fun apply(isAlive: Boolean, neighbors: Int): Boolean =
         if (isAlive) neighbors in listOf(3, 6, 7, 8) else neighbors in listOf(3, 6, 7, 8)
 }
+
+data object HighLifeRule : Rule {
+    override fun apply(isAlive: Boolean, neighbors: Int): Boolean =
+        if (isAlive) neighbors in 2..3 else neighbors == 3 || neighbors == 6
+}
--- app/src/main/kotlin/org/redbyte/life/ui/settings/SharedGameSettingsViewModel.kt
+++ app/src/main/kotlin/org/redbyte/life/ui/settings/SharedGameSettingsViewModel.kt
@@ -7,6 +7,7 @@ import org.redbyte.life.common.domain.ClassicRule
 import org.redbyte.life.common.GameBoard
 import org.redbyte.life.common.data.GameSettings
 import org.redbyte.life.common.domain.DayAndNightRule
+import org.redbyte.life.common.domain.HighLifeRule
 
 class SharedGameSettingsViewModel : ViewModel() {
     private var _gameBoard: GameBoard? = null
@@ -21,14 +22,14 @@ class SharedGameSettingsViewModel : ViewModel() {
     fun resetGameBoard(newSettings: GameSettings? = null) {
         val gameSettings =
             newSettings ?: settings.value ?: throw RuntimeException("Game settings cannot be null")
-        _gameBoard = GameBoard(gameSettings, DayAndNightRule)
+        _gameBoard = GameBoard(gameSettings, HighLifeRule)
     }
 
     fun getGameBoard(): GameBoard = _gameBoard ?: resetGameBoardAndGet()
 
     private fun resetGameBoardAndGet(): GameBoard {
         val gameSettings = settings.value ?: throw RuntimeException("Game settings cannot be null")
-        _gameBoard = GameBoard(gameSettings, DayAndNightRule)
+        _gameBoard = GameBoard(gameSettings, HighLifeRule)
         return _gameBoard as GameBoard
     }
 }
